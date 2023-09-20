c5124e7a99d8d04998e01334e05b3e741b8fba60
Mia-F <mia.frivik@gmail.com>
2023-09-20 11:52:24 +0200

    writing

--- code/part_f.py
+++ code/part_f.py
@@ -1,1 +1,18 @@
+"""
+Cross-validation as resampling techniques
+"""
 
+import numpy as np
+from part_a import Franke_function, design_matrix, MSE, R2, beta_OLS
+import matplotlib.pyplot as plt
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from matplotlib import cm
+from sklearn.model_selection import train_test_split
+import time
+from tqdm import tqdm
+
+def k_fold(data):
+    """
+    Takes in data and splits it in to k different segments....
+    """
+    
