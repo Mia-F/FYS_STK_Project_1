c8d3c61641731052ac1bfcb3292f3212e91879a9
Janita Willumsen <j.willu@me.com>
2023-10-04 14:16:45 +0200

    Test bootstrap for ridge, messy code but it works!

--- code/part_e.py
+++ code/part_e.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from imageio.v2 import imread
 
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator
@@ -11,10 +12,13 @@ from sklearn.pipeline import make_pipeline
 from sklearn.utils import resample
 from sklearn.metrics import mean_squared_error
 
+from matplotlib import cm
+from tqdm import tqdm
+
 # from part_a import Franke_function, design_matrix
 
 
-def Franke_function(x,y, noise=False, noise_factor=0):
+def Franke_function(x,y, noise_factor=0):
     """
     Franke_function returns an array with dimension (len(x), len(y)), if noise is set to true
     the output will contain noise given by a gaussian distribution N(0,1)
@@ -65,6 +69,10 @@ def beta_OLS(X, y):
   """
 
   return np.linalg.pinv(X.T @ X) @ X.T @ y
+
+
+def beta_ridge(X, y, lamb):
+    return (np.linalg.pinv(X.T @ X + lamb * np.identity(len(X[0]))) @ X.T @ y)
 
 
 def franke_function(x, y, noise=False, noise_factor=0.1):
@@ -163,7 +171,7 @@ def bootstrap_reshape(x, y, z, degree, num_bootstraps):
     return (error_train, error_test, bias, variance)
 
 
-def bootstrap(x, y, z, degree, num_bootstraps):
+def bootstrap_ols(x, y, z, degree, num_bootstraps):
     """Resampling data using bootstrap algorithm.
     
     Args:
@@ -193,7 +201,45 @@ def bootstrap(x, y, z, degree, num_bootstraps):
         z_tilde[:, i] = (X_ @ beta).ravel() 
         z_predict[:, i] = (X_test @ beta).ravel() 
 
-        mse_train[i] = mse(z_train, z_tilde[:, i])
+        mse_train[i] = mse(z_, z_tilde[:, i])
+        mse_test[i] = mse(z_test, z_predict[:, i])
+
+    error_train = np.mean(mse_train)
+    error_test = np.mean(mse_test)
+    bias = np.mean((z_test - np.mean(z_predict, axis=1))**2)
+    variance = np.mean(np.var(z_predict, axis=1, keepdims=True))
+
+    return (error_train, error_test, bias, variance)
+
+
+def bootstrap_ridge(X_train, X_test, z_train, z_test, lamb, num_bootstraps):
+    """Resampling data using bootstrap algorithm.
+    
+    Args:
+        x (np.ndarray): x-values
+        y (np.ndarray): y-values
+        z (np.ndarray): values of Franke function
+        degree (int): polynomial degree
+        num_bootstraps (int): number of resamples
+        
+    Returns:
+        tuple: Error of train and test, bias and variance
+    """
+    z_tilde = np.empty((z_train.shape[0], num_bootstraps))
+    z_predict = np.empty((z_test.shape[0], num_bootstraps))
+
+    mse_train = np.empty(num_bootstraps)
+    mse_test = np.empty(num_bootstraps)
+
+    for i in range(num_bootstraps):
+        X_, z_ = resample(X_train, z_train)
+        beta = beta_ridge(X_, z_, lamb)
+        # beta = beta_ols(X_, z_)
+
+        z_tilde[:, i] = (X_ @ beta).ravel() 
+        z_predict[:, i] = (X_test @ beta).ravel() 
+
+        mse_train[i] = mse(z_, z_tilde[:, i])
         mse_test[i] = mse(z_test, z_predict[:, i])
 
     error_train = np.mean(mse_train)
@@ -249,31 +295,76 @@ def bootstrap_sklearn(x, y, z, degree, num_bootstraps, intercept=True):
     return (np.mean(error), error_train, error_test, bias, variance)
 
 
+def creating_data(data_file, n):
+    new_data = np.zeros((n,n))
+    terrain1 = imread(data_file)
+    for i in tqdm(range(n)):
+        for j in range(n):
+            new_data[i][j] = terrain1[i][j]
+    return new_data
+
+    
+def load_real_data(n):
+    # Load the terrain
+    filename = '../DataFiles/SRTM_data_Norway_1.tif'
+    data = creating_data(filename, n)
+
+    #creating x and y
+    n,m = data.shape
+
+    x = np.sort(np.random.uniform(0, 1, n))
+    y = np.sort(np.random.uniform(0, 1, m))
+
+    x,y = np.meshgrid(x,y)
+
+
+    x_1D = x.ravel()
+    y_1D = y.ravel()
+    z = data.ravel()
+
+    return (x_1D, y_1D, z)
+
+
+def ols_error_plot(x, y, z, max_degree):
+    degrees = np.zeros(max_degree)
+    error_train = np.zeros(max_degree)
+    error_test = np.zeros(max_degree)
+    bias = np.zeros(max_degree)
+    variance = np.zeros(max_degree)
+
+    for i in range(max_degree):
+        degrees[i] = i + 1
+        error_train[i], error_test[i], bias[i], variance[i] = bootstrap_ols(x, y, z, i+1, 100)
+
+    fig, ax = plt.subplots()
+    ax.plot(degrees, error_train, label='Train error')
+    ax.plot(degrees, error_test, label='Test error')
+    ax.plot(degrees, bias, 'b--', label='Bias')
+    ax.plot(degrees, variance, 'r--', label='Variance')
+    ax.set_yscale('log')
+    ax.legend()
+
 if __name__ == '__main__':
     np.random.seed(2023)
     n = 20
-    max_degree = 20
+    max_degree = 15
+    degrees = np.arange(1, max_degree+1, 1, dtype=np.int32)
+
+
     x_ = np.linspace(0, 1, n)
     y_ = np.linspace(0, 1, n)
 
     x, y = np.meshgrid(x_, y_)
-    
-    # x = np.ravel(x)
-    # y = np.ravel(y)
 
-    # With reshape
-    # z = franke_function(x, y, noise=True, noise_factor=1)
-    # z = z.reshape(-1, 1)
-    # plot_3d(x, y, z)
-
+    # Franke
     x.ravel()
     y.ravel()
-    z = Franke_function(x, y, noise=True, noise_factor=1)
+    z = Franke_function(x, y, noise_factor=1)
 
-    # SKlearn
-    # z = Franke_function(x.ravel(), y.ravel(), noise=True, noise_factor=0)
+    # Real data
+    # x, y, z = load_real_data(n)
 
-    degrees = np.zeros(max_degree)
+    # degrees = np.zeros(max_degree)
     error = np.zeros(max_degree)
     error_train = np.zeros(max_degree)
     error_test = np.zeros(max_degree)
@@ -281,26 +372,52 @@ if __name__ == '__main__':
     variance = np.zeros(max_degree)
 
 
-    for i in range(max_degree):
-        degrees[i] = i + 1
-        error_train[i], error_test[i], bias[i], variance[i] = bootstrap(x, y, z, i+1, 100)
+    # for i in range(max_degree):
+    #     degrees[i] = i + 1
+    #     error_train[i], error_test[i], bias[i], variance[i] = bootstrap_ols(x, y, z, i+1, 100)
+        # error_train[i], error_test[i], bias[i], variance[i] = bootstrap(x, y, z, i+1, 100)
         # error_train[i], error_test[i], bias[i], variance[i] = bootstrap_reshape(x, y, z, i+1, 100)
         # error[i], error_train[i], error_test[i], bias[i], variance[i] = bootstrap_sklearn(x, y, z, i+1, 100, intercept=False)
 
-    error_sum = bias + variance
-    fig, ax = plt.subplots()
-    # ax.plot(degrees, np.log10(error_train), label='Train')
-    # ax.plot(degrees, np.log10(error_test), label='Test')
-    # ax.plot(degrees, np.log10(bias), 'b--', label='Bias')
-    # ax.plot(degrees, np.log(variance), 'r--', label='Variance')
+    # error_sum = bias + variance
+    # fig, ax = plt.subplots()
 
-    # ax.plot(degrees, error_train, label='Train')
-    # ax.plot(degrees, error_sum, label='Error sum')
-    ax.plot(degrees, error_test, label='Test error')
-    ax.plot(degrees, bias, 'b--', label='Bias')
-    ax.plot(degrees, variance, 'r--', label='Variance')
+    # ax.plot(degrees, error_train, label='Train error')
+    # ax.plot(degrees, error_test, label='Test error')
+    # ax.plot(degrees, bias, 'b--', label='Bias')
+    # ax.plot(degrees, variance, 'r--', label='Variance')
     # ax.set_xscale('log')
+    # ax.set_yscale('log')
+    # ax.legend()
+    # plt.show()
+
+    lambdas = np.logspace(-8, 0, 9)
+    l = len(lambdas)
+
+    error_lamb = np.zeros((len(degrees), l))
+    error_train_lamb = np.zeros((len(degrees), l))
+    error_test_lamb = np.zeros((len(degrees), l))
+    bias_lamb = np.zeros((len(degrees), l))
+    variance_lamb = np.zeros((len(degrees), l))
+
+    # Ridge bootstrap
+    for i in range(max_degree):
+        X = design_matrix(x, y, int(degrees[i]))
+        X_train, X_test, z_train, z_test = train_test_split(X, z.flatten(), test_size=0.2)
+
+        for j in tqdm(range(l)):
+            error_train_lamb[i, j], error_test_lamb[i, j], bias_lamb[i, j], variance_lamb[i, j] = bootstrap_ridge(X_train, X_test, z_train, z_test, lambdas[j], 100)
+
+    e_train = np.zeros(max_degree)
+
+    fig, ax = plt.subplots()    
+    for j in range(l):
+        # e_train = error_train_lamb[:, j]
+        ax.plot(degrees, error_train_lamb[:, j], label=f'Train error {lambdas[j]}')
+        ax.plot(degrees, error_test_lamb[:, j], label=f'Test error {lambdas[j]}')
+        ax.plot(degrees, bias_lamb[:, j], label=f'Bias {lambdas[j]}')
+        ax.plot(degrees, variance_lamb[:, j], label=f'Variance {lambdas[j]}')
+        # ax.set_xscale('log')
     ax.set_yscale('log')
     ax.legend()
     plt.show()
-
