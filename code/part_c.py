edaa2f3ba4f8e2a6aefbbee0b18216b7d71d01cf
AMyrvang <andrmyrv@mail.uio.no>
2023-10-09 11:41:00 +0200

    adding a plot

--- code/part_c.py
+++ code/part_c.py
@@ -4,16 +4,31 @@ Implementing Lasso regression
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from part_a import Franke_function, design_matrix, MSE, R2
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn import linear_model
 from tqdm import tqdm
 
+sns.set_theme()
+params = {
+    "font.family": "Serif",
+    "font.serif": "Roman", 
+    "text.usetex": True,
+    "axes.titlesize": "large",
+    "axes.labelsize": "large",
+    "xtick.labelsize": "large",
+    "ytick.labelsize": "large",
+    "legend.fontsize": "large"}
+plt.rcParams.update(params)
+
+
 np.random.seed(2023)
 n = 50
 degree = np.linspace(0,5,6, dtype=int)
-alphas  = np.logspace(-8, 2, 1000)
+#alphas  = np.logspace(-8, 2, 1000)
+alphas = [0.0001, 0.001, 0.01, 0.1, 1.0]
 
 
 x = np.sort(np.random.uniform(0, 1, n))
@@ -52,7 +67,7 @@ for d in range(len(degree)):
         r2_score_Lasso_train[d][i] = R2(y_train, model_train)
         mse_error_Lasso_test[d][i] = MSE(y_test, model_test)
         r2_score_Lasso_test[d][i] = R2(y_test, model_test)
-
+"""
 plt.title(r"Heatmap of the MSE for the training data as a fucntion of $\lambda$ values and complexity")
 plt.imshow(mse_error_Lasso_train, aspect='auto')
 plt.grid()
@@ -175,6 +190,32 @@ plt.show()
 
 plt.plot(np.log10(alphas), mse_error_Lasso_train, label="MSE train")
 plt.plot(np.log10(alphas), mse_error_Lasso_test, label="MSE test")
+plt.show()
+"""
+# Plotting MSE vs Degrees for different alpha values.
+plt.figure(figsize=(15, 8))
+for i, alpha in enumerate(alphas):
+    plt.plot(degree, mse_error_Lasso_train[:, i], label=rf'$\lambda$ = $10^{{{int(np.log10(alpha))}}}$', linestyle=':')
+    plt.plot(degree, mse_error_Lasso_test[:, i], label=rf'$\lambda$ = $10^{{{int(np.log10(alpha))}}}$', linestyle='-' )
+
+plt.xlabel('Degree')
+plt.ylabel('Mean Squared Error (MSE)')
+plt.title('MSE vs. Degrees for different Alpha values (Lasso)')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# Plotting R2 vs Degrees for different alpha values.
+plt.figure(figsize=(12, 6))
+for i, alpha in enumerate(alphas):
+    plt.plot(degree, r2_score_Lasso_train[:, i], label=rf'$\lambda$ = $10^{{{int(np.log10(alpha))}}}$', linestyle=':' )
+    plt.plot(degree, r2_score_Lasso_test[:, i], label=rf'$\lambda$ = $10^{{{int(np.log10(alpha))}}}$', linestyle='-' )
+
+plt.xlabel('Degree')
+plt.ylabel('R-squared (R2)')
+plt.title('R2 vs. Degrees for different Alpha values (Lasso)')
+plt.legend()
+plt.grid(True)
 plt.show()
 
 
