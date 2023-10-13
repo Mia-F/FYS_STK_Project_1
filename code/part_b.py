bd55c93679046552f9e3ccecbed038239036e064
Mia-F <mia.frivik@gmail.com>
2023-10-13 11:42:15 +0200

    coding

--- code/part_b.py
+++ code/part_b.py
@@ -16,15 +16,30 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error
 
+fontsize = 30
+sns.set_theme()
+params = {
+    "font.family": "Serif",
+    "font.serif": "Roman", 
+    "text.usetex": True,
+    "axes.titlesize": fontsize,
+    "axes.labelsize": fontsize,
+    "xtick.labelsize": "large",
+    "ytick.labelsize": "large",
+    "legend.fontsize": fontsize
+}
+plt.rcParams.update(params)
+
 np.random.seed(2023)
 
 def beta_ridge(X, y, lamb):
     return (np.linalg.pinv(X.T @ X + lamb * np.identity(len(X[0]))) @ X.T @ y)
 
     
-n = 50
+n = 20
 degree = np.linspace(0,5,6, dtype=int)
 lambdas = np.logspace(-8, 2, 1000)
+
 
 
 x = np.sort(np.random.uniform(0, 1, n))
@@ -35,7 +50,7 @@ x,y = np.meshgrid(x,y)
 x = np.ravel(x)
 y = np.ravel(y)
 
-f = Franke_function(x,y, noise=True)
+f = Franke_function(x,y,noise=True)
 
 
 Error_train = np.zeros((len(degree), len(lambdas)))
@@ -45,7 +60,6 @@ R2_score_test = np.zeros((len(degree), len(lambdas)))
 
 
 for d in tqdm(range(len(degree))):
-    print(d)
     X = design_matrix(x,y, degree[d])
     X_train, X_test, y_train, y_test = train_test_split(X, f, test_size=0.2) 
     for i in range(len(lambdas)):
@@ -60,8 +74,43 @@ for d in tqdm(range(len(degree))):
         R2_score_test[d][i] = R2(y_test, model_test)
 
 
+
+Error_train_lam = np.zeros(len(degree))
+Error_test_lam = np.zeros(len(degree))
+
+R2_train_lam = np.zeros(len(degree))
+R2_test_lam = np.zeros(len(degree))
+
+for i in range(len(degree)):
+    Error_train_lam[i] = Error_train[i][0]
+    Error_test_lam [i]= Error_test[i][0]
+
+    R2_train_lam[i] = R2_score_train[i][0]
+    R2_test_lam[i] = R2_score_test[i][0]
+
+
+#Plotting MSE and R2 score
+fig, ax = plt.subplots()
+plot_1 = ax.plot(degree, Error_train_lam[:], label="MSE Train", color="tab:orange")
+plot_2 = ax.plot(degree, Error_test_lam, label="MSE Test", color="tab:blue")
+ax.set_ylabel("MSE")
+ax.set_xlabel("Degrees")
+ 
+
+ax2 = ax.twinx()
+plot_3 = ax2.plot(degree, R2_train_lam, label="R2 Train", color="tab:green")
+plot_4 = ax2.plot(degree, R2_test_lam, label="R2 Test", color="tab:red")
+ax2.set_ylabel("R2 score")
+plt.grid()
+  
+
+lns = plot_1 + plot_2 + plot_3 +plot_4
+labels = [l.get_label() for l in lns]
+plt.legend(lns, labels, loc="center right")
+plt.show()
+
 #Plotting heatmaps for lambda dependency
-plt.title(r"Heatmap of the MSE for the training data as a fucntion of $\lambda$ values and complexity")
+#plt.title(r"Heatmap of the MSE for the training data as a fucntion of $\lambda$ values and complexity")
 plt.imshow(Error_train, aspect='auto')
 plt.grid()
 plt.xlabel(r"$\lambda$ values from $10^{-8}$ to $10^{2}$")
@@ -69,7 +118,7 @@ plt.ylabel("Degree")
 plt.colorbar(label="MSE")
 plt.show()
 
-plt.title(r"Heatmap of the MSE for the test data as a fucntion of $\lambda$ values and complexity")
+#plt.title(r"Heatmap of the MSE for the test data as a fucntion of $\lambda$ values and complexity")
 plt.imshow(Error_test, aspect='auto')
 plt.grid()
 plt.xlabel(r"$\lambda$ values from $10^{-5}$ to $10^{5}$")
@@ -101,7 +150,7 @@ plt.show()
 
 #Plottig 6 3D subplots of diffrent fitings of franke function
 n=100
-lambdas = 10e-5
+lambdas = 10e-8
 x = np.sort(np.random.uniform(0, 1, n))
 y = np.sort(np.random.uniform(0, 1, n))
 x,y = np.meshgrid(x,y)
@@ -133,3 +182,68 @@ for i in range(len(degree)):
         ax.set_title(f"Degree = {i}", fontsize = 20)
 
 plt.show()
+
+n = 20
+degree = np.linspace(0,5,6, dtype=int)
+lambdas = 10e-8
+#lambdas = np.array([0])
+
+
+x = np.sort(np.random.uniform(0, 1, n))
+y = np.sort(np.random.uniform(0, 1, n))
+
+x,y = np.meshgrid(x,y)
+
+x = np.ravel(x)
+y = np.ravel(y)
+
+f = Franke_function(x,y,noise=True)
+
+beta_val = np.zeros(len(degree), dtype=object)
+	
+beta_vals = []
+beta_vals_mean = []
+beta_vals_std = []
+
+for d in tqdm(range(len(degree))):
+    X = design_matrix(x,y, degree[d])
+    X_train, X_test, y_train, y_test = train_test_split(X, f, test_size=0.2) 
+    
+    beta = beta_ridge(X_train, y_train, lambdas)
+
+    beta_vals.append(np.mean(beta, axis= 1))
+    beta_vals_degree = np.mean(beta, axis=1)
+    beta_vals_mean.append(beta_vals_degree)
+    beta_vals_std.append(np.std(beta, axis=1))
+
+    model_train = X_train @ beta
+    model_test = X_test @ beta
+       
+color_palette = sns.color_palette("tab10", len(degree))
+	
+# Plotting beta coefficients for different degrees
+plt.figure()
+for i in range(len(degree)):
+    plt.plot(range(len(beta_vals[i])), beta_vals[i], marker = "o" ,label=f'Degree {degree[i]}', color=color_palette[i])
+
+locs, labels = plt.xticks()  # Get the current locations and labels.
+#plt.title(r"Plot of the $\beta$ values for the different polynomial degrees")
+plt.xticks(np.arange(0, 1, step=1))  # Set label locations.
+plt.xticks(np.arange(21), [r'$\beta_0$', r'$\beta_1$', r'$\beta_2$', \
+        r'$\beta_3$', r'$\beta_4$', r'$\beta_5$', \
+        r'$\beta_6$', r'$\beta_7$', r'$\beta_8$', \
+        r'$\beta_9$', r'$\beta_{10}$', r'$\beta_{11}$', \
+        r'$\beta_{12}$', r'$\beta_{13}$', r'$\beta_{14}$', \
+        r'$\beta_{15}$', r'$\beta_{16}$', r'$\beta_{17}$', \
+        r'$\beta_{18}$', r'$\beta_{19}$', r'$\beta_{20}$'\
+        ], rotation=45)  # Set text labels.
+
+plt.xlabel("Coefficient Index")
+plt.ylabel("Beta Coefficient Value")
+plt.legend()
+plt.show()
+      
+
+
+
+
