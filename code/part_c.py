bf41f18974fcad27c09caf400bda5c92d74dabe7
AMyrvang <142980660+AMyrvang@users.noreply.github.com>
2023-10-14 11:04:49 +0200

    Cleaned up the code part_c.py

--- code/part_c.py
+++ code/part_c.py
@@ -25,10 +25,12 @@ params = {
 }
 plt.rcParams.update(params)
 
+
 np.random.seed(2023)
 n = 20
-degree = np.array([0,1,2,3])
-#degree = np.array([9])
+degree = np.array([0, 1, 2, 3, 4, 5])
+#alphas = np.logspace(-8,2,1000)
+alphas = np.logspace(-5, -3, 3)
 
 x = np.sort(np.random.uniform(0, 1, n))
 y = np.sort(np.random.uniform(0, 1, n))
@@ -40,7 +42,6 @@ y = np.ravel(y)
 
 f = Franke_function(x, y, noise = True)
 
-alphas = np.logspace(-8,2,100)
 
 mse_error_Lasso_train = np.zeros((len(degree),len(alphas)))
 r2_score_Lasso_train = np.zeros((len(degree),len(alphas)))
@@ -57,18 +58,19 @@ for d in range(len(degree)):
   
     for i, alpha in tqdm(enumerate(alphas)):
         # Create and fit the Lasso regression model
-        clf = linear_model.Lasso(fit_intercept=True, max_iter=1000000, alpha=alpha)
+        #clf = linear_model.Lasso(fit_intercept=True, max_iter=1000000, alpha=alpha)
+        clf = linear_model.Lasso(fit_intercept=True, max_iter=10000, alpha=alpha)
         clf.fit(X_train, y_train)
 
         model_train = clf.predict(X_train)
         model_test = clf.predict(X_test)
 
         mse_error_Lasso_train[d][i] = MSE(y_train, model_train)
-        #print(mse_error_Lasso_train[i])
         r2_score_Lasso_train[d][i] = R2(y_train, model_train)
         mse_error_Lasso_test[d][i] = MSE(y_test, model_test)
         r2_score_Lasso_test[d][i] = R2(y_test, model_test)
 
+"""
 #plt.title(r"Heatmap of the MSE for the training data as a fucntion of $\lambda$ values and complexity")
 plt.imshow(mse_error_Lasso_train, aspect='auto')
 plt.grid()
@@ -84,8 +86,11 @@ plt.xlabel(r"$\lambda$ values from $10^{-8}$ to $10^{2}$")
 plt.ylabel("Degree")
 plt.colorbar(label="MSE")
 plt.show()
+"""
+
 #best_alpha = alphas[np.argmin(mse_error_Lasso_test)]
 #print("Best alpha:", best_alpha)
+
 #Plotting MSE and R2 score
 fig, ax = plt.subplots()
 plot_1 = ax.plot(degree, mse_error_Lasso_train[:,0], label="MSE Train", color="tab:orange")
@@ -93,130 +98,13 @@ plot_2 = ax.plot(degree, mse_error_Lasso_test[:,0], label="MSE Test", color="tab
 ax.set_ylabel("MSE")
 ax.set_xlabel("Degrees")
  
-
 ax2 = ax.twinx()
 plot_3 = ax2.plot(degree, r2_score_Lasso_train[:,0], label="R2 Train", color="tab:green")
 plot_4 = ax2.plot(degree, r2_score_Lasso_test[:,0], label="R2 Test", color="tab:red")
 ax2.set_ylabel("R2 score")
 plt.grid()
   
-
 lns = plot_1 + plot_2 + plot_3 +plot_4
 labels = [l.get_label() for l in lns]
 plt.legend(lns, labels, loc="center right")
 plt.show()
-
-
-
-plt.plot(degree, mse_error_Lasso_train[:,0])
-plt.plot(degree, mse_error_Lasso_test[:,0])
-plt.show()
-
-
-# Plot the results
-fig, ax = plt.subplots()
-ax.semilogx(alphas, mse_error_Lasso_train[0][:], label="MSE Lasso (Train)", color="tab:blue")
-ax.semilogx(alphas, r2_score_Lasso_train[0][:], label="R2 Lasso (Train)", color="tab:orange")
-ax.semilogx(alphas, mse_error_Lasso_test[0][:], label="MSE Lasso (Test)", color="tab:green")
-ax.semilogx(alphas, r2_score_Lasso_test[0][:], label="R2 Lasso (Test)", color="tab:red")
-ax.set_title("MSE and R2 scores for Lasso with different polynomial degrees")
-ax.set_xlabel("Alphas")
-ax.set_ylabel("MSE and R2 values")
-ax.legend()
-plt.show()
-fig, ax = plt.subplots()
-ax.semilogx(alphas, mse_error_Lasso_train[1][:], label="MSE Lasso (Train)", color="tab:blue")
-ax.semilogx(alphas, r2_score_Lasso_train[1][:], label="R2 Lasso (Train)", color="tab:orange")
-ax.semilogx(alphas, mse_error_Lasso_test[1][:], label="MSE Lasso (Test)", color="tab:green")
-ax.semilogx(alphas, r2_score_Lasso_test[1][:], label="R2 Lasso (Test)", color="tab:red")
-ax.set_title("MSE and R2 scores for Lasso with different polynomial degrees")
-ax.set_xlabel("Alphas")
-ax.set_ylabel("MSE and R2 values")
-ax.legend()
-plt.show()
-fig, ax = plt.subplots()
-ax.semilogx(alphas, mse_error_Lasso_train[2][:], label="MSE Lasso (Train)", color="tab:blue")
-ax.semilogx(alphas, r2_score_Lasso_train[2][:], label="R2 Lasso (Train)", color="tab:orange")
-ax.semilogx(alphas, mse_error_Lasso_test[2][:], label="MSE Lasso (Test)", color="tab:green")
-ax.semilogx(alphas, r2_score_Lasso_test[2][:], label="R2 Lasso (Test)", color="tab:red")
-ax.set_title("MSE and R2 scores for Lasso with different polynomial degrees")
-ax.set_xlabel("Alphas")
-ax.set_ylabel("MSE and R2 values")
-ax.legend()
-plt.show()
-fig, ax = plt.subplots()
-ax.semilogx(alphas, mse_error_Lasso_train[3][:], label="MSE Lasso (Train)", color="tab:blue")
-ax.semilogx(alphas, r2_score_Lasso_train[3][:], label="R2 Lasso (Train)", color="tab:orange")
-ax.semilogx(alphas, mse_error_Lasso_test[3][:], label="MSE Lasso (Test)", color="tab:green")
-ax.semilogx(alphas, r2_score_Lasso_test[3][:], label="R2 Lasso (Test)", color="tab:red")
-ax.set_title("MSE and R2 scores for Lasso with different polynomial degrees")
-ax.set_xlabel("Alphas")
-ax.set_ylabel("MSE and R2 values")
-ax.legend()
-plt.show()
-fig, ax = plt.subplots()
-ax.semilogx(alphas, mse_error_Lasso_train[4][:], label="MSE Lasso (Train)", color="tab:blue")
-ax.semilogx(alphas, r2_score_Lasso_train[4][:], label="R2 Lasso (Train)", color="tab:orange")
-ax.semilogx(alphas, mse_error_Lasso_test[4][:], label="MSE Lasso (Test)", color="tab:green")
-ax.semilogx(alphas, r2_score_Lasso_test[4][:], label="R2 Lasso (Test)", color="tab:red")
-ax.set_title("MSE and R2 scores for Lasso with different polynomial degrees")
-ax.set_xlabel("Alphas")
-ax.set_ylabel("MSE and R2 values")
-ax.legend()
-plt.show()
-fig, ax = plt.subplots()
-ax.semilogx(alphas, mse_error_Lasso_train[5][:], label="MSE Lasso (Train)", color="tab:blue")
-ax.semilogx(alphas, r2_score_Lasso_train[5][:], label="R2 Lasso (Train)", color="tab:orange")
-ax.semilogx(alphas, mse_error_Lasso_test[5][:], label="MSE Lasso (Test)", color="tab:green")
-ax.semilogx(alphas, r2_score_Lasso_test[5][:], label="R2 Lasso (Test)", color="tab:red")
-ax.set_title("MSE and R2 scores for Lasso with different polynomial degrees")
-ax.set_xlabel("Alphas")
-ax.set_ylabel("MSE and R2 values")
-ax.legend()
-plt.show()
-fig, ax = plt.subplots()
-ax.semilogx(alphas, mse_error_Lasso_train[6][:], label="MSE Lasso (Train)", color="tab:blue")
-ax.semilogx(alphas, r2_score_Lasso_train[6][:], label="R2 Lasso (Train)", color="tab:orange")
-ax.semilogx(alphas, mse_error_Lasso_test[6][:], label="MSE Lasso (Test)", color="tab:green")
-ax.semilogx(alphas, r2_score_Lasso_test[6][:], label="R2 Lasso (Test)", color="tab:red")
-ax.set_title("MSE and R2 scores for Lasso with different polynomial degrees")
-ax.set_xlabel("Alphas")
-ax.set_ylabel("MSE and R2 values")
-ax.legend()
-plt.show()
-fig, ax = plt.subplots()
-ax.semilogx(alphas, mse_error_Lasso_train[7][:], label="MSE Lasso (Train)", color="tab:blue")
-ax.semilogx(alphas, r2_score_Lasso_train[7][:], label="R2 Lasso (Train)", color="tab:orange")
-ax.semilogx(alphas, mse_error_Lasso_test[7][:], label="MSE Lasso (Test)", color="tab:green")
-ax.semilogx(alphas, r2_score_Lasso_test[7][:], label="R2 Lasso (Test)", color="tab:red")
-ax.set_title("MSE and R2 scores for Lasso with different polynomial degrees")
-ax.set_xlabel("Alphas")
-ax.set_ylabel("MSE and R2 values")
-ax.legend()
-plt.show()
-fig, ax = plt.subplots()
-ax.semilogx(alphas, mse_error_Lasso_train[8][:], label="MSE Lasso (Train)", color="tab:blue")
-ax.semilogx(alphas, r2_score_Lasso_train[8][:], label="R2 Lasso (Train)", color="tab:orange")
-ax.semilogx(alphas, mse_error_Lasso_test[8][:], label="MSE Lasso (Test)", color="tab:green")
-ax.semilogx(alphas, r2_score_Lasso_test[8][:], label="R2 Lasso (Test)", color="tab:red")
-ax.set_title("MSE and R2 scores for Lasso with different polynomial degrees")
-ax.set_xlabel("Alphas")
-ax.set_ylabel("MSE and R2 values")
-ax.legend()
-plt.show()
-fig, ax = plt.subplots()
-ax.semilogx(alphas, mse_error_Lasso_train[9][:], label="MSE Lasso (Train)", color="tab:blue")
-ax.semilogx(alphas, r2_score_Lasso_train[9][:], label="R2 Lasso (Train)", color="tab:orange")
-ax.semilogx(alphas, mse_error_Lasso_test[9][:], label="MSE Lasso (Test)", color="tab:green")
-ax.semilogx(alphas, r2_score_Lasso_test[9][:], label="R2 Lasso (Test)", color="tab:red")
-ax.set_title("MSE and R2 scores for Lasso with different polynomial degrees")
-ax.set_xlabel("Alphas")
-ax.set_ylabel("MSE and R2 values")
-ax.legend()
-plt.show()
-
-plt.plot(np.log10(alphas), mse_error_Lasso_train, label="MSE train")
-plt.plot(np.log10(alphas), mse_error_Lasso_test, label="MSE test")
-plt.show()
-
-
